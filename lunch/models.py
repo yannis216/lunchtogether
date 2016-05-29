2a8d68bc2d6addd4890682d7470c007ce963e53a
yannis216 <yannisbeuke@googlemail.com>
2016-05-29 10:25:23 +0200

    Models of lunch request

---
+++ lunch/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from  django.utils import timezone
+# Create your models here.
+
+class Lunchrequest(models.Model):
+    user = models.CharField(max_length=50)
+    place = models.CharField(max_length=100)
+    time = models.DateTimeField()
+    email = models.EmailField()
+
+
+    def __str__(self):
+        return self.user
